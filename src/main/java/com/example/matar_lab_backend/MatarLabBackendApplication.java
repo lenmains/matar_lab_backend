8bd39eaebcbb74c3d9ecc740cab61101c8c4c098
詹詹 <50549207+liushuchen2025@users.noreply.github.com>
2023-09-05 06:03:02 -0400

    Initial commit

---
+++ src/main/java/com/example/matar_lab_backend/MatarLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.matar_lab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatarLabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatarLabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/matar_lab_backend/MatarLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matar_lab_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatarLabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
